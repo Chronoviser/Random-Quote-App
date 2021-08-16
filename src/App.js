ba89524a63953e7dde4fb0ec5aea9f428174848d
chronoviser <abhisheksinghoriginal@gmail.com>
2021-08-16 18:29:43 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [quote, setQuote] = useState("");
+
+    function fetchQuote() {
+        axios
+            .get("https://api.adviceslip.com/advice")
+            .then((response) => {
+                setQuote(response.data.slip.advice);
+            })
+            .catch((e) => {
+                setQuote("Oops... No Network :(")
+            });
+    }
+
+    useEffect(() => {
+        fetchQuote();
+    }, [])
+
+    return (
+        <div className="app">
+            <div className="card">
+                <h2 className="heading">{quote}</h2> <br />
+                <button className="button" onClick={fetchQuote}><span>Give me an Advice</span></button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
